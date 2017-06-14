2fd7cc3639f08df755da8202b054e159229f41e7
Emmanuel Victor Garcia <emmanuel.garcia@xurpas.com>
2017-06-14 12:19:16 +0800

    Removed sticker overlay

---
+++ app/src/main/java/team/uptech/motionviews/MainActivity.java
@@ -0,0 +1,249 @@
+package team.uptech.motionviews;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+import com.xeleb.motionviews.ui.TextEditorDialogFragment;
+import com.xeleb.motionviews.ui.adapter.FontsAdapter;
+import com.xeleb.motionviews.utils.FontProvider;
+import com.xeleb.motionviews.viewmodel.Font;
+import com.xeleb.motionviews.viewmodel.TextLayer;
+import com.xeleb.motionviews.widget.MotionView;
+import com.xeleb.motionviews.widget.entity.MotionEntity;
+import com.xeleb.motionviews.widget.entity.TextEntity;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TextEditorDialogFragment.OnTextLayerCallback {
+
+    protected MotionView motionView;
+    protected View textEntityEditPanel;
+    private final MotionView.MotionViewCallback motionViewCallback = new MotionView.MotionViewCallback() {
+        @Override
+        public void onEntitySelected(@Nullable MotionEntity entity) {
+            if (entity instanceof TextEntity) {
+                textEntityEditPanel.setVisibility(View.VISIBLE);
+            } else {
+                textEntityEditPanel.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onEntityDoubleTap(@NonNull MotionEntity entity) {
+            startTextEntityEditing();
+        }
+    };
+    private FontProvider fontProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.fontProvider = new FontProvider(getResources());
+
+        motionView = (MotionView) findViewById(R.id.main_motion_view);
+        textEntityEditPanel = findViewById(R.id.main_motion_text_entity_edit_panel);
+        motionView.setMotionViewCallback(motionViewCallback);
+
+        initTextEntitiesListeners();
+    }
+
+    private void initTextEntitiesListeners() {
+        findViewById(R.id.text_entity_font_size_increase).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseTextEntitySize();
+            }
+        });
+        findViewById(R.id.text_entity_font_size_decrease).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decreaseTextEntitySize();
+            }
+        });
+        findViewById(R.id.text_entity_color_change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTextEntityColor();
+            }
+        });
+        findViewById(R.id.text_entity_font_change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTextEntityFont();
+            }
+        });
+        findViewById(R.id.text_entity_edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTextEntityEditing();
+            }
+        });
+    }
+
+    private void increaseTextEntitySize() {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity != null) {
+            textEntity.getLayer().getFont().increaseSize(TextLayer.Limits.FONT_SIZE_STEP);
+            textEntity.updateEntity();
+            motionView.invalidate();
+        }
+    }
+
+    private void decreaseTextEntitySize() {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity != null) {
+            textEntity.getLayer().getFont().decreaseSize(TextLayer.Limits.FONT_SIZE_STEP);
+            textEntity.updateEntity();
+            motionView.invalidate();
+        }
+    }
+
+    private void changeTextEntityColor() {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity == null) {
+            return;
+        }
+
+        int initialColor = textEntity.getLayer().getFont().getColor();
+
+        ColorPickerDialogBuilder
+                .with(MainActivity.this)
+                .setTitle(R.string.select_color)
+                .initialColor(initialColor)
+                .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+                .density(8) // magic number
+                .setPositiveButton(R.string.ok, new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        TextEntity textEntity = currentTextEntity();
+                        if (textEntity != null) {
+                            textEntity.getLayer().getFont().setColor(selectedColor);
+                            textEntity.updateEntity();
+                            motionView.invalidate();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
+
+    private void changeTextEntityFont() {
+        final List<String> fonts = fontProvider.getFontNames();
+        FontsAdapter fontsAdapter = new FontsAdapter(this, fonts, fontProvider);
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.select_font)
+                .setAdapter(fontsAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        TextEntity textEntity = currentTextEntity();
+                        if (textEntity != null) {
+                            textEntity.getLayer().getFont().setTypeface(fonts.get(which));
+                            textEntity.updateEntity();
+                            motionView.invalidate();
+                        }
+                    }
+                })
+                .show();
+    }
+
+    private void startTextEntityEditing() {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity != null) {
+            TextEditorDialogFragment fragment = TextEditorDialogFragment.getInstance(textEntity.getLayer().getText());
+            fragment.show(getFragmentManager(), TextEditorDialogFragment.class.getName());
+        }
+    }
+
+    @Nullable
+    private TextEntity currentTextEntity() {
+        if (motionView != null && motionView.getSelectedEntity() instanceof TextEntity) {
+            return ((TextEntity) motionView.getSelectedEntity());
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        addTextSticker();
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void addTextSticker() {
+        TextLayer textLayer = createTextLayer();
+        TextEntity textEntity = new TextEntity(textLayer, motionView.getWidth(),
+                motionView.getHeight(), fontProvider);
+        motionView.addEntityAndPosition(textEntity);
+
+        // move text sticker up so that its not hidden under keyboard
+        PointF center = textEntity.absoluteCenter();
+        center.y = center.y * 0.5F;
+        textEntity.moveCenterTo(center);
+
+        // redraw
+        motionView.invalidate();
+
+        startTextEntityEditing();
+    }
+
+    private TextLayer createTextLayer() {
+        TextLayer textLayer = new TextLayer();
+        Font font = new Font();
+
+        font.setColor(TextLayer.Limits.INITIAL_FONT_COLOR);
+        font.setSize(TextLayer.Limits.INITIAL_FONT_SIZE);
+        font.setTypeface(fontProvider.getDefaultFontName());
+
+        textLayer.setFont(font);
+
+        if (BuildConfig.DEBUG) {
+            textLayer.setText("Text overlay");
+        }
+
+        return textLayer;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void textChanged(@NonNull String text) {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity != null) {
+            TextLayer textLayer = textEntity.getLayer();
+            if (!text.equals(textLayer.getText())) {
+                textLayer.setText(text);
+                textEntity.updateEntity();
+                motionView.invalidate();
+            }
+        }
+    }
+}
