410f41672478b45cdb310cf76dd746f53c2b65f9
dev-emman <emmanuel.garcia@xurpas.com>
2017-06-21 19:03:03 +0800

    Migrated to support repository

--- motion-views/src/main/java/com/xeleb/motionviews/ui/TextEditorDialogFragment.java
+++ motion-views/src/main/java/com/xeleb/motionviews/ui/TextEditorDialogFragment.java
@@ -80,16 +80,16 @@ public class TextEditorDialogFragment extends DialogFragment {
 //        }
 //    }
 //
-//    @Override
-//    public void onAttachFragment(Fragment childFragment) {
-//        super.onAttachFragment(childFragment);
-//        if (childFragment.getActivity() instanceof OnTextLayerCallback) {
-//            this.callback = (OnTextLayerCallback) childFragment.getActivity();
-//        } else {
-//            throw new IllegalStateException(childFragment.getActivity().getClass().getName()
-//                    + " must implement " + OnTextLayerCallback.class.getName());
-//        }
-//    }
+    @Override
+    public void onAttachFragment(Fragment childFragment) {
+        super.onAttachFragment(childFragment);
+        if (childFragment.getActivity() instanceof OnTextLayerCallback) {
+            this.callback = (OnTextLayerCallback) childFragment.getActivity();
+        } else {
+            throw new IllegalStateException(childFragment.getActivity().getClass().getName()
+                    + " must implement " + OnTextLayerCallback.class.getName());
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
